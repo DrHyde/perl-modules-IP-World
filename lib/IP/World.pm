af582014bd6adbc80051f2e6562f360dfeabb4fe
Craig MacKenna <craigm014@gmail.com>
2010-04-08 06:34:11 +0000

    Import of MACKENNA/IP-World-0.31 from CPAN.
    gitpan-cpan-distribution: IP-World
    gitpan-cpan-version:      0.31
    gitpan-cpan-path:         MACKENNA/IP-World-0.31.tar.gz
    gitpan-cpan-author:       MACKENNA
    gitpan-cpan-maturity:     released

--- lib/IP/World.pm
+++ lib/IP/World.pm
@@ -13,7 +13,7 @@ require DynaLoader;
 # Exporter is not needed because we're object-oriented
 our @ISA = qw(DynaLoader);
 
-our $VERSION = '0.3';
+our $VERSION = '0.31';
 
 sub new {
     my ($pkg, $mode) = @_;
