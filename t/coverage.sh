c0a5a43e09ccb17dfbc36566c7bfa0817967d480
David Cantrell <david@cantrell.org.uk>
2016-10-27 22:49:51 +0100

    try to report coverage of the XS code

--- t/coverage.sh
+++ t/coverage.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+
 cover -delete
+perl Build.PL --extra-compiler-flags "-O0 --coverage" --extra-linker-flags "--coverage"
 HARNESS_PERL_SWITCHES=-MDevel::Cover ./Build test
-cover
+cover -ignore_re /CORE/\\w+\\.h$
