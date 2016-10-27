68433a59c400fb3c0f8aeadeb0c0f1a00fa39152
David Cantrell <david@cantrell.org.uk>
2016-10-27 23:23:21 +0100

    exclude nonsense

--- t/coverage.sh
+++ t/coverage.sh
@@ -3,4 +3,4 @@
 cover -delete
 perl Build.PL --extra-compiler-flags "-O0 --coverage" --extra-linker-flags "--coverage"
 HARNESS_PERL_SWITCHES=-MDevel::Cover ./Build test
-cover -ignore_re /CORE/\\w+\\.h$
+cover -ignore_re \(Builder\|/CORE/\\w+\\\.h$\)
